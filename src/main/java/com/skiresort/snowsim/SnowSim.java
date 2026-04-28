080f1771ae7e772805586eafa7b29b651a631400
octaneDev <96917601+Octane-dev@users.noreply.github.com>
2026-04-28 22:43:10 +0100

    Update SnowSim.java

--- src/main/java/com/skiresort/snowsim/SnowSim.java
+++ src/main/java/com/skiresort/snowsim/SnowSim.java
@@ -4,11 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SnowSim extends JavaPlugin {
 
+    // Cached deltas from /snowsample, keyed by reference point name
+    // Each entry is the delta in layers to add (positive) or remove (negative)
+    private int[] cachedDeltas = null;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getCommand("updatesnow").setExecutor(new UpdateSnowCommand(this));
-        
+        getCommand("snowsample").setExecutor(new SnowSampleCommand(this));
+        getCommand("snowapply").setExecutor(new SnowApplyCommand(this));
         getLogger().info("SnowSim enabled.");
     }
 
@@ -16,4 +21,7 @@ public class SnowSim extends JavaPlugin {
     public void onDisable() {
         getLogger().info("SnowSim disabled.");
     }
+
+    public int[] getCachedDeltas() { return cachedDeltas; }
+    public void setCachedDeltas(int[] deltas) { this.cachedDeltas = deltas; }
 }
